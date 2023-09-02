827611dfa4f32f368829c115434c18b180f19a16
DHughes666 <graziemolto6@gmail.com>
2023-09-02 17:51:19 +0100

    Sign up initiated

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
+
+
+const SignUpForm = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields)
+
+    return (
+       <div>
+        <h1>Sign up with your email and password</h1>
+        <form onSubmit={() => {}}>
+            <label>Name</label>
+            <input type="text" required/>
+
+            <label>Email</label>
+            <input type="email" required/>
+
+            <label>Password</label>
+            <input type="password" required/>
+
+            <label>Confirm Password</label>
+            <input type="password" required/>
+
+            <button type="submit">Sign Up</button>
+        </form>
+       </div> 
+    )
+}
+
+export default SignUpForm;
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,31 +1,11 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
 
-import { auth, signInWithGooglePopup, signInWithGoogleRedirect,
+import { signInWithGooglePopup, 
     createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 
 const SignIn = () => {
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await getRedirectResult(auth);
-                if (response) {
-                    const userDocRef = await createUserDocumentFromAuth(response.user);
-                }
-            } catch (err) {
-                console.log(err);
-            }
-
-            const response = await getRedirectResult(auth);
-            if (response) {
-                const userDocRef = await createUserDocumentFromAuth(response.user);
-            }
-        }
-
-        fetchData()
-    }, [])
-
+    
     const popUpGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
@@ -35,7 +15,7 @@ const SignIn = () => {
         <div>
             <h1>Sign In Page</h1>
             <button onClick={popUpGoogleUser}>Sign in with Google Popup</button>
-            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
+            <SignUpForm />
         </div>
     )
 }
