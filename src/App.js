146b611437bd9c7f81efa7a5daaabee78e3e1559
DHughes666 <graziemolto6@gmail.com>
2023-09-02 02:22:37 +0100

    Outlet and Link - Routing explained

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
 
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home.component';
+
 const App = ()  => {
 
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
 
 export default App;
+
