216fc85bb76a45b30f0325bc910b6ea68067a3d3
DHughes666 <graziemolto6@gmail.com>
2023-09-02 02:54:10 +0100

    Sign In feature initiated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 import Home from './routes/home.component';
 
 const App = ()  => {
@@ -9,6 +10,7 @@ const App = ()  => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,16 +1,22 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
+import './navigation.styles.scss';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 
 const Navigation = () => {
     return (
       <Fragment>
         <div className="navigation">
           <Link className="logo-container" to='/'>
-            <div>Logo</div>
+            <CrwnLogo className="logo" />
           </Link>
           <div className="nav-links-container">
             <Link className="nav-link" to='/shop'>
                 Shop
+            </Link>
+            <Link className="nav-link" to='/sign-in'>
+                Sign In
             </Link>
           </div>
         </div>
